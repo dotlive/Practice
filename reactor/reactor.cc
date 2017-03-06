7469a76679668a0ea5a943d69ccbac500618b130
dotlive <dotlive@163.com>
2017-03-06 22:41:57 +0800

    test-select & epoll

---
+++ reactor/reactor.cc
@@ -0,0 +1,137 @@
+#include <assert.h>
+#include "reactor.h"
+#include "event_demultiplexer.h"
+
+/// @file   reactor.cc
+/// @brief  Reactor类实现
+
+namespace reactor
+{
+/// reactor的实现类
+class ReactorImplementation
+{
+public:
+
+    /// 构造函数
+    ReactorImplementation();
+
+    /// 析构函数
+    ~ReactorImplementation();
+
+    /// 向reactor中注册关注事件evt的handler
+    /// @param  handler 要注册的事件处理器
+    /// @param  evt     要关注的事件
+    /// @retval =0      注册成功
+    /// @retval <0      注册出错
+    int RegisterHandler(EventHandler * handler, event_t evt);
+
+    /// 从reactor中移除handler
+    /// @param  handler 要移除的事件处理器
+    /// @retval =0      移除成功
+    /// @retval <0      移除出错
+    int RemoveHandler(EventHandler * handler);
+
+    /// 处理事件,回调注册的handler中相应的事件处理函数
+    /// @param  timeout 超时时间
+    void HandleEvents(int timeout);
+
+private:
+
+    EventDemultiplexer *                m_demultiplexer; ///< 事件多路分发器
+    std::map<handle_t, EventHandler *>  m_handlers;      ///< 所有handler集合
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+/// 构造函数
+Reactor::Reactor()
+{
+    m_reactor_impl = new ReactorImplementation();
+}
+
+/// 析构函数
+Reactor::~Reactor()
+{
+    delete m_reactor_impl;
+}
+
+/// 向reactor中注册关注事件evt的handler
+/// @param  handler 要注册的事件处理器
+/// @param  evt     要关注的事件
+/// @retval =0      注册成功
+/// @retval <0      注册出错
+int Reactor::RegisterHandler(EventHandler * handler, event_t evt)
+{
+    return m_reactor_impl->RegisterHandler(handler, evt);
+}
+
+/// 从reactor中移除handler
+/// @param  handler 要移除的事件处理器
+/// @retval =0      移除成功
+/// @retval <0      移除出错
+int Reactor::RemoveHandler(EventHandler * handler)
+{
+    return m_reactor_impl->RemoveHandler(handler);
+}
+
+/// 处理事件,回调注册的handler中相应的事件处理函数
+/// @param  timeout 超时时间
+void Reactor::HandleEvents(int timeout)
+{
+    m_reactor_impl->HandleEvents(timeout);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+/// 构造函数
+ReactorImplementation::ReactorImplementation()
+{
+#if defined(_WIN32)
+    m_demultiplexer = new SelectDemultiplexer();
+#elif defined(__linux__)
+    m_demultiplexer = new EpollDemultiplexer();
+#else
+#error "目前还不支持该平台"
+#endif // _WIN32
+}
+
+/// 析构函数
+ReactorImplementation::~ReactorImplementation()
+{
+    delete m_demultiplexer;
+}
+
+/// 向reactor中注册关注事件evt的handler
+/// @param  handler 要注册的事件处理器
+/// @param  evt     要关注的事件
+/// @retval =0      注册成功
+/// @retval <0      注册出错
+int ReactorImplementation::RegisterHandler(EventHandler * handler, event_t evt)
+{
+    handle_t handle = handler->GetHandle();
+    std::map<handle_t, EventHandler *>::iterator it = m_handlers.find(handle);
+    if (it == m_handlers.end())
+    {
+        m_handlers[handle] = handler;
+    }
+    return m_demultiplexer->RequestEvent(handle, evt);
+}
+
+/// 从reactor中移除handler
+/// @param  handler 要移除的事件处理器
+/// @retval =0      移除成功
+/// @retval <0      移除出错
+int ReactorImplementation::RemoveHandler(EventHandler * handler)
+{
+    handle_t handle = handler->GetHandle();
+    m_handlers.erase(handle);
+    return m_demultiplexer->UnrequestEvent(handle);
+}
+
+/// 处理事件,回调注册的handler中相应的事件处理函数
+/// @param  timeout 超时时间
+void ReactorImplementation::HandleEvents(int timeout)
+{
+    m_demultiplexer->WaitEvents(&m_handlers);
+}
+} // namespace reactor
---
+++ reactor/reactor_client_test.cc
@@ -0,0 +1,154 @@
+#ifndef REACTOR_TIME_CLIENT_H_
+#define REACTOR_TIME_CLIENT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <assert.h>
+#include <errno.h>
+#ifdef __linux__
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif
+#include "test_common.h"
+
+/// @file   reactor_client_test.cc
+/// @brief  用reactor实现的客户端,用telnet协议
+
+/// reactor对象
+reactor::Reactor g_reactor;
+
+/// 读/写数据用到的buffer
+const size_t kBufferSize = 1024;
+char g_read_buffer[kBufferSize];
+char g_write_buffer[kBufferSize];
+
+class TimeClient : public reactor::EventHandler
+{
+public:
+
+    /// 构造函数
+    TimeClient() :
+        EventHandler()
+    {
+        m_handle = socket(AF_INET, SOCK_STREAM, 0);
+        assert(IsValidHandle(m_handle));
+    }
+
+    /// 析构函数
+    ~TimeClient()
+    {
+        close(m_handle);
+    }
+
+    /// 连接服务器
+    bool ConnectServer(const char * ip, unsigned short port)
+    {
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip);
+        if (connect(m_handle, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+        {
+            ReportSocketError("connect");
+            return false;
+        }
+        return true;
+    }
+
+    /// 获取handle
+    virtual reactor::handle_t GetHandle() const
+    {
+        return m_handle;
+    }
+
+    /// 处理从server收到的应答
+    virtual void HandleRead()
+    {
+        memset(g_read_buffer, 0, kBufferSize);
+        int len = recv(m_handle, g_read_buffer, kBufferSize, 0);
+        if (len > 0)
+        {
+            fprintf(stderr, "%s", g_read_buffer);
+            g_reactor.RegisterHandler(this, reactor::kWriteEvent);
+        }
+        else
+        {
+            ReportSocketError("recv");
+        }
+    }
+
+    /// 向server发请求
+    virtual void HandleWrite()
+    {
+        memset(g_write_buffer, 0, kBufferSize);
+        int len = sprintf(g_write_buffer, "time\r\n");
+        len = send(m_handle, g_write_buffer, len, 0);
+        if (len > 0)
+        {
+            fprintf(stderr, "%s", g_write_buffer);
+            g_reactor.RegisterHandler(this, reactor::kReadEvent);
+        }
+        else
+        {
+            ReportSocketError("send");
+        }
+    }
+
+    /// 处理出错
+    virtual void HandleError()
+    {
+        fprintf(stderr, "server closed\n");
+        close(m_handle);
+        exit(0);
+    }
+
+private:
+
+    reactor::handle_t  m_handle;  ///< 客户端句柄
+};
+
+int main(int argc, char ** argv)
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "usage: %s ip port\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+#ifdef _WIN32
+    WSADATA wsa_data;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+    {
+        fprintf(stderr, "WSAStartup() error:%d\n", WSAGetLastError());
+        assert(0);
+    }
+#endif
+
+    TimeClient client;
+    if (!client.ConnectServer(argv[1], atoi(argv[2])))
+    {
+        fprintf(stderr, "connect remote server failed\n");
+        return EXIT_FAILURE;
+    }
+
+    g_reactor.RegisterHandler(&client, reactor::kWriteEvent);
+    while (1)
+    {
+        g_reactor.HandleEvents(100);
+#if defined(_WIN32)
+        Sleep(1000);
+#elif defined(__linux__)
+        sleep(1);
+#endif
+    }
+    g_reactor.RemoveHandler(&client);
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return EXIT_SUCCESS;
+}
+
+#endif // REACTOR_TIME_CLIENT_H_
